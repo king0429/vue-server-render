1090f879402107e6e367d66de609ac5ae2ee19e5
jinhang <jinhang@ninstarscf.com>
2018-12-05 18:10:47 +0800

    add packages

---
+++ components/index/board.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="board">
+    <ul>
+      <li
+        v-for="(item, index) in showTitle"
+        :key="index"
+        class="index_"
+      >
+        <p>{{ item }}</p>
+        <p>{{ dataList[index] }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "IndexBoard",
+  props: {
+    dataList: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+  data: () => ({
+    showTitle: ['第一次', '第二次']
+  })
+}
+</script>
+<style lang="less" scoped>
+  #board {
+    height: 800px;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li {
+      list-style: none;
+      width: 400px;
+      height: 160px;
+      border: 1px solid #acacac;
+      margin: 0 10px;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,22 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        server-side-render
-      </h1>
-      <h2 class="subtitle">
-        sql-express-template-html
-      </h2>
-      <div class="links">
-        <nuxt-link to="/data_table">SQL</nuxt-link>
-        <a
-          href="./test"
-          target="_blank"
-          class="button--green">test</a>
-      </div>
-    </div>
-  </section>
+  <div class="container">
+    <IndexBoard />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import IndexBoard from '~/components/index/board.vue'
 
 export default {
   components: {
-    Logo
-  }
+    IndexBoard
+  },
+  data () {}
 }
 </script>
 
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
